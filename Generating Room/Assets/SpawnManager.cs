712f79709718c95e8c4862506cdad438568b160f
charlesDouc <charles.douc@gmail.com>
2018-04-17 17:48:23 -0400

    Set the room. Set the spawning positions. Initialized the spawn manager script.

---
+++ Generating Room/Assets/SpawnManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+
+	// public variables
+
+	// private variables
+
+	// ------------------------------------
+	// Use this for initialization
+	// ------------------------------------
+	void Start () {
+		
+	}
+
+	// ------------------------------------
+	// Update is called once per frame
+	// ------------------------------------
+	void Update () {
+		
+	}
+
+	// ------------------------------------
+	// Methods
+	// ------------------------------------
+
+}
